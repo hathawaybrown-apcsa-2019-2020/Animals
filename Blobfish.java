b93fcb459ae7c06ebe68b4a88010617678c6a34c
Isabel <ihsieh21@hb.edu>
2019-10-01 14:53:20 -0400

    have a blobfish!!
    Isabel created the blobfish

---
+++ Blobfish.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Blobfish here.
+ *
+ * @author Isabel
+ * @version 2019-10-01
+ */
+public class Blobfish extends Animal
+{
+    public void eat()
+    {
+        System.out.println("tiny mollusks");
+    }
+
+    public void speak()
+    {
+        System.out.println("blurb");
+    }
+}
