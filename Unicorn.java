ac1eb03b01e14d988226022d53384efcd64659f5
suzy <sschwabl22@hb.edu>
2019-10-01 14:53:47 -0400

    suzy and carolyn created UNICORN

---
+++ Unicorn.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Unicorn here.
+ *
+ * @author Suzy
+ * @version 2019-10-01
+ */
+public class Unicorn
+{
+    public void eat()
+    {
+        System.out.print("chocolate");
+    }
+    public void speak()
+    {
+        System.out.print("metal clanking");
+    }
+}
