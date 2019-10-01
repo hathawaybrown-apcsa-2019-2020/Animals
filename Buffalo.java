dc158356eee4c2aa0079fb711394040f7f684a9b
Maxine Gilligan <mgilligan21@hb.edu>
2019-10-01 14:53:13 -0400

    Maxine created a buffalo

---
+++ Buffalo.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Unicorn here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Buffalo
+{
+    public void eat()
+    {
+        System.out.println("grass");
+    }
+    
+    public void speak()
+    {
+        System.out.println("* silence *");
+    }
+}
