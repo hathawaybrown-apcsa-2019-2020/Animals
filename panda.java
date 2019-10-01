81d9ab8f67e272337f0fc8f0a29d58212e4d46c0
suzy <sschwabl22@hb.edu>
2019-10-01 15:15:02 -0400

    suzy created panda

---
+++ panda.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class panda here.
+ *
+ * @author Suzy
+ * @version 2019-10-01
+ */
+public class panda
+{
+public void eat()
+    {
+        System.out.print("chocolate");
+    }
+    public void speak()
+    {
+        System.out.print("metal clanking");
+    }
+}
